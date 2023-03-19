675ad7095a10fb96748a6c3aa25ec8bb3532be28
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-03-19 18:32:04 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+
 export const App = () => {
   return (
     <div
       style={{
+        textAlign: 'center',
         height: '100vh',
-        display: 'flex',
+
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-      React homework template good
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Домашня </div>} />
+          <Route path="dogs" element={<div>Собаки</div>} />
+          <Route path="dogs/:dogId" element={<div>Елемент колекції</div>} />
+        </Route>
+      </Routes>
     </div>
   );
 };
